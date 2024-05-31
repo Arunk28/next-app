0faa7b9480fe7e3d269f0a8274891ca2a8d3cacc
Arunk28 <=arunkumar280491@gmail.com>
2024-05-31 17:20:38 +0530

    html add on added

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,13 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    html: {
+      prettier: {
+        tabWidth: 4,
+        useTabs: false,
+        htmlWhitespaceSensitivity: "strict",
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
